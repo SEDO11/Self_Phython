3e0bb5998c0e8cb9d81d7f27e19a8d0ee929d349
SEDO11 <do11anm@naver.com>
2022-03-09 19:52:13 +0900

    220309

--- 백준/사칙연산/exam2588.py
+++ 백준/사칙연산/exam2588.py
@@ -1,9 +1,9 @@
 a = input()
 b = input()
 
-result1 = int(a) * int(b[2:])
-result2 = int(a) * int(b[1:2])
-result3 = int(a) * int(b[:1])
+result1 = int(a) * int(b[2:]) #첫번째 숫자
+result2 = int(a) * int(b[1:2]) #두번째 숫자
+result3 = int(a) * int(b[:1]) #세 번째 숫자
 result4 = int(a) * int(b)
 
 print(result1)
