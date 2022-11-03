b13029870b1f03194e9cfaed145caa6a93ad529f
SEDO11 <do11anm@naver.com>
2022-11-04 01:26:26 +0900

    221103

---
+++ 이코테/복습/5주차dfs_bfs/연산자끼워넣기(연습필요).py
@@ -0,0 +1,13 @@
+#연산자 끼워넣기
+
+import sys
+input = sys.stdin.readline
+
+n = input()
+a = list(map(int, input().split()))
+c = list(map(int, input().split()))
+cnt = sum(c)
+r = []
+
+def dfs(a, c):
+    
